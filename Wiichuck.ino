1eea579b0eded3256f33940837845ccfc47e3de1
Rodolfo Cossovich <todocono@gmail.com>
2016-11-15 14:15:27 +0800

    compatibility for OEM added

--- Wiichuck.ino
+++ Wiichuck.ino
@@ -3,49 +3,57 @@
  *
  * Animatronics - NYU Shanghai
  * Prof. Rodolfo Cossovich
- * 
+ *
  * Original code from Tod E. Kurt, http://thingm.com/
+ * Code works for both original WII Nunchuck and OEM joysticks
  *
  */
 
 #include <Wire.h>
 #include "nunchuck_funcs.h"
 
-int loop_cnt=0;
+int loop_cnt = 0;
 
-byte accx,accy,zbut,cbut;
-int ledPin = 13;
-
+byte accX, accY, accZ, joyX, joyY, joyZ, butZ, butC;
 
 void setup()
 {
-    delay (1000);
-    Serial.begin(9600);
-    nunchuck_setpowerpins();
-    delay (1000);
-    nunchuck_init(); // send the initilization handshake
-    delay (1000);
-    Serial.print("WiiChuckDemo ready\n");
+  delay (1000);
+  Serial.begin(9600);
+  nunchuck_setpowerpins();
+  nunchuck_init(); // send the initilization handshake
+  Serial.print("WiiChuck ready to transmit ... \n");
 }
 
 void loop()
 {
-    if( loop_cnt > 100 ) { // every 100 msecs get new data
-        loop_cnt = 0;
+  if ( loop_cnt > 100 ) { // every 100 msecs get new data
+    loop_cnt = 0;
 
-        nunchuck_get_data();
+    nunchuck_get_data();
 
-        accx  = nunchuck_accelx(); // ranges from approx 70 - 182
-        accy  = nunchuck_accely(); // ranges from approx 65 - 173
-        zbut = nunchuck_zbutton();
-        cbut = nunchuck_cbutton(); 
-            
-        Serial.print("accx: "); Serial.print((byte)accx,DEC);
-        Serial.print("\taccy: "); Serial.print((byte)accy,DEC);
-        Serial.print("\tzbut: "); Serial.print((byte)zbut,DEC);
-        Serial.print("\tcbut: "); Serial.println((byte)cbut,DEC);
-    }
-    loop_cnt++;
-    delay(1);
+    joyX  = nunchuck_joyx(); // ranges from approx 70 - 182
+    joyY  = nunchuck_joyy(); // ranges from approx 65 - 173
+    
+
+    accX  = nunchuck_accelx(); // ranges from approx 70 - 182
+    accY  = nunchuck_accely(); // ranges from approx 65 - 173
+    accZ  = nunchuck_accelz(); // ranges from approx 65 - 173
+    
+    butZ = nunchuck_zbutton();
+    butC = nunchuck_cbutton();
+
+    Serial.print("joyX: "); Serial.print((byte)joyX, DEC);
+    Serial.print("  joyY: "); Serial.print((byte)joyY, DEC);
+
+    Serial.print("  accX: "); Serial.print((byte)accX, DEC);
+    Serial.print("  accY: "); Serial.print((byte)accY, DEC);
+    Serial.print("  accZ: "); Serial.print((byte)accZ, DEC);
+    
+    Serial.print("  butZ: "); Serial.print((byte)butZ, DEC);
+    Serial.print("  butC: "); Serial.println((byte)butC, DEC);
+  }
+  loop_cnt++;
+  delay(1);
 }
 
